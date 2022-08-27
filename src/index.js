d3b6585a6c93d0ae500598d303574cdd9b5dd8fa
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-27 14:14:08 -0400

    Installed Babbel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
 
 
 var motionQuery = matchMedia("(prefers-reduced-motion)");
--- src/lotties.js
+++
@@ -1,7 +0,0 @@
-var animation = lottie.loadAnimation({
-  container: document.getElementById("anima"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "meAnimation.json",
-});
